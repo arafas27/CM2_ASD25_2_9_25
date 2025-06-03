434fc434a0711066bad35f75ce549bab11b1aedd
YanuarRamadhaniKuswoko <ryanuar867@gmail.com>
2025-06-03 12:13:50 +0700

    Class Transaksi Pengisian

---
+++ TransaksiPengisian.java
@@ -0,0 +1,15 @@
+public class TransaksiPengisian {
+    Kendaraan kendaraan;
+    BBM bbm;
+    double liter;
+    double totalBayar;
+    TransaksiPengisian next;
+
+    public TransaksiPengisian(Kendaraan kendaraan, BBM bbm, double liter) {
+        this.kendaraan = kendaraan;
+        this.bbm = bbm;
+        this.liter = liter;
+        this.totalBayar = bbm.hargaPerLiter * liter;
+        this.next = null;
+    }
+}
